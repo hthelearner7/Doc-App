4829d55a8ce481f48b9d1f2fb794c90b1fe3a707
hthelearner7 <153205804+hthelearner7@users.noreply.github.com>
2024-08-02 17:47:13 +0530

    Update db.js
    removed a log statement

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectToDB = async () => {
     try {
         console.log("mongodb");
         await mongoose.connect(process.env.MONGO_URL);
-        console.log(`MongoDB connected ${mongoose.connection.host}`.bgGreen.white);
+        console.log("MongoDB connected successfully");
 
     } catch (e) {
         console.log(`MongoDB Server Issue ${e}`.bgRed.white);
