73fa66a7b4f80b2a40015264ab1319dedd3b47f5
hthelearner7 <himanshujuyal280404@gmail.com>
2024-08-02 16:20:51 +0530

    completed-code
    Doc App Success

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const colors = require('colors');
+
+const connectToDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log(`MongoDB connected ${mongoose.connection.host}`.bgGreen.white);
+
+    } catch (e) {
+        console.log(`MongoDB Server Issue ${e}`.bgRed.white);
+    }
+}
+
+module.exports = connectToDB;
