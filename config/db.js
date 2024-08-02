f6f75a3c289e5d98c6b2e95076350804d3710e04
hthelearner7 <153205804+hthelearner7@users.noreply.github.com>
2024-08-02 17:38:57 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const colors = require('colors');
 
 const connectToDB = async () => {
     try {
+        console.log("mongodb");
         await mongoose.connect(process.env.MONGO_URL);
         console.log(`MongoDB connected ${mongoose.connection.host}`.bgGreen.white);
 
